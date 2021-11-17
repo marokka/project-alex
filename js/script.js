16b1b1241116a499392043961648a67bf666ddd1
Alexander <exampele@gmail.com>
2021-11-17 19:59:29 +0300

    1 lesson

--- js/script.js
+++ js/script.js
@@ -43,12 +43,28 @@
 
 // console.log(isChecked || !isClose); 
 
-const numberOfFilms = prompt("Сколько фильмов вы уже смотрели?","");
+// ------- Первый урок
 
-console.log(numberOfFilms);
+// const numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?","");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: 
-};
+// console.log(numberOfFilms);
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//       b = prompt("На сколько оцените его?",""),
+//       c = prompt("Один из последних просмотренных фильмов?", ""),
+//       d = prompt("На сколько оцените его?","");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+// --------- конец
       
