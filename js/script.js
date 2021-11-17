07792d43768afe9ae1ce7d7af61153da54f01c70
Alexander <exampele@gmail.com>
2021-11-17 21:03:36 +0300

    2 lesson

--- js/script.js
+++ js/script.js
@@ -66,5 +66,100 @@
 // personalMovieDB.movies[c] = d;
 
 // console.log(personalMovieDB);
-// --------- конец
-      
+// --------- конец------------------------------------------------------------------------------------------------------------
+
+// -------Условия-----
+// if (1){
+//     console.log("OK");
+// }else {
+//     console.log("Error");
+// }
+
+// const num = 50;
+
+// if (num<49){
+//     console.log("ErroR!!!");
+// } else if (num>100){
+//     console.log("Много");
+// }else {
+//     console.log("OK");
+// }
+  
+// (num === 50) ?  console.log("OK") : console.log("ErroR!!!");
+
+
+// const num = 51;
+// switch(num) {
+
+//     case 49: console.log("bad");
+//     break;
+    
+//     case 100: console.log("big");
+//     break;
+
+//     case 50: console.log("Nice");
+//     break;
+
+//     default: console.log("ban");
+//     break;
+
+// }
+// ------------End------------------
+
+// ------------Циклы----------------
+// let num = 50;
+
+// // while(num<=55){
+// //     console.log(num);
+// //     num++;
+// // }
+
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+// // while (num<=55);
+
+// for(let i = 1;i<=10;i++){
+//     if(i===6){
+//         break;// break - останавливает цикл, так же есть continue - он позволяет пропустить не желательный шаг но приэтом дает возможность циклу проделать весь процесс 
+//     }    
+//     console.log(i);
+// }
+// ------- Второй урок-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?","");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//       b = prompt("На сколько оцените его?",""),
+//       c = prompt("Один из последних просмотренных фильмов?", ""),
+//       d = prompt("На сколько оцените его?","");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++){
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?","");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("Save!!!");
+    } else {
+        console.log("Error!!!");
+        i--;
+        
+    }
+          
+}
+
+
+console.log(personalMovieDB);
