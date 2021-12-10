97d951969e7bcb3921c359b5ad639f3039b7fef2
Alexander <exampele@gmail.com>
2021-12-10 17:23:32 +0300

    my commit

--- js/script.js
+++ js/script.js
@@ -513,3 +513,112 @@
 // console.log();
 
 //----------------------------------------------END--------------------------------------------------
+
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function(){
+//         console.log('Hello');
+//     }
+// };
+// const jonh = Object.create(soldier); // создание прототипных связей 
+
+
+// // const jonh = {
+// //     health: 100
+// // };
+
+// // jonh.__proto__ = soldier --- получаем свойство прототипа, из объекта, это усторевший формат
+
+// // Object.setPrototypeOf(jonh, soldier) --- это современный метод для получения свойст прототипа
+
+
+// // console.log(jonh.sayHello());
+// jonh.sayHello();// взяли функцию из прототипа.
+
+
+//---------------------------------------Практика за прошедшие уроки--------------
+
+
+// const personalMovieDB = {
+//     count: 0,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: true,
+//     start: function() {
+//         personalMovieDB.count = +prompt("Сколько фильмов вы уже смотрели?","");
+        
+//             while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+//                 personalMovieDB.count = +prompt("Сколько фильмов вы уже смотрели?","");
+//             }
+//     },
+//     rememberMyFilms: function(){ 
+//             for (let i = 0; i < 2; i++){
+//                 const a = prompt("Один из последних просмотренных фильмов?", ""),
+//                     b = prompt("На сколько оцените его?","");
+        
+//                 if (a!=null && b!=null && a!='' && b!='' && a.length<50) {
+//                     personalMovieDB.movies[a] = b;
+//                     console.log("Save!!!");
+//                 } else {
+//                     console.log("Error!!!");
+//                     i--;
+                    
+//                 }     
+//             }
+//     },
+//     detectPersonalLevel: function(){
+//             if (personalMovieDB.count<10)
+//             {
+//                 alert("Просмотрено достаточно мало фильмов");
+//             } else if(personalMovieDB.count>=10 && personalMovieDB.count<30)
+//             {
+//                 alert("Вы классический зритель");
+//             }else if(personalMovieDB.count>=30)
+//             {
+//                 alert("Вы киноман");
+//             } else {
+//                 alert("Error");
+//             }
+//     },
+//     showMyDB: function(hidden){
+//             if(!hidden){
+//                 console.log(personalMovieDB);
+//             }
+//     },
+//     writeYourGenres: function() {
+//             for(let i=1;i < 2;i++){
+//                 // let genre = prompt(`Ваш любимый жанр под номером ${i}`,"");
+//                 // personalMovieDB.genres[i-1] = genre;
+
+//                 // if (genre == '' || genre == null){
+//                 //    alert('Запоните данные!!!');
+//                 //    i--;
+//                 // }else{
+//                 //     personalMovieDB.genres[i-1] = genre;
+//                 // } 
+//                 let genres = prompt(`Введите ваши любимые жандры через запятую`,"");
+//                 if (genres == '' || genres == null){
+//                    alert('Запоните данные!!!');
+//                    i--;
+//                 }else{
+//                     personalMovieDB.genres = genres.split(', ');
+//                     personalMovieDB.genres.sort();
+//                 } 
+//             }
+//             personalMovieDB.genres.forEach((item, i) =>{
+//                 console.log(`Любимый жанр ${i+1} - это ${item}`);
+//             });
+
+//     },
+//     toggleVisibleMyDB: function(){
+//         if(personalMovieDB.privat){
+//             personalMovieDB.privat = false;
+//         }else{
+//             personalMovieDB.privat = true;
+//         }
+//     }
+// };
+
+// personalMovieDB.writeYourGenres();
