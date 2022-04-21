935aa4409765683ac0e18bc059f905d9cfc4ff67
Alexander <exampele@gmail.com>
2022-04-21 11:14:58 +0300

    new

--- js/script_new.js
+++ js/script_new.js
@@ -1,21 +1,2 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы посмотрели","");
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
-
-const a = prompt("Последний фильм какой вы смотрели??"),
-      b = prompt("Оцените фильм"),
-      c = prompt("Последний фильм какой вы смотрели??"),
-      d = prompt("Оцените фильм");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
