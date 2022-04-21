11612cc0844c692a61dbbb0c5cc7b2318fda38fc
Alexander <exampele@gmail.com>
2022-04-21 11:49:09 +0300

    new2

--- js/script_new.js
+++ js/script_new.js
@@ -1,2 +1,38 @@
 "use strict";
 
+const numberOfFilms = prompt("Сколько фильмов вы посмотрели","");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+if (personalMovieDB.count<10){
+    alert("Просмотрено довольно мало фильмов");
+}else if(personalMovieDB.count>=10 && personalMovieDB.count<30){
+    alert("Вы классический зритель");
+}else if(personalMovieDB.count>=30){
+    alert("Вы киноман");
+}else{
+    alert("Error");
+}
+
+for(let i = 0; i < 2; i++){
+    const a = prompt("Последний фильм какой вы смотрели??"),
+          b = prompt("Оцените фильм");
+    if (a != null && b != null && a!='' && b!='' && a.length<50)
+    {
+        personalMovieDB.movies[a] = b;
+    }else{
+        alert('Error!!!');
+        i--;
+    }
+
+}
+
+console.log(personalMovieDB);
+
+
