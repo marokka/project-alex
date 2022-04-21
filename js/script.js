b574bb899aa5e66e417575115bb6b9c76d7f552c
Alexander <exampele@gmail.com>
2022-04-21 10:46:43 +0300

    new file

--- js/script.js
+++ js/script.js
@@ -753,3 +753,440 @@
 // // div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>'); втавляет в начало элемента
 // // div.insertAdjacentHTML('beforeend', '<h2>Hello</h2>'); вставляет конец элемента
 // // div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');//вставит четко после элементом
+
+//---------------------------------------------Работа с элементами (практика)---------------------------
+
+// const movieDB = {
+//     movies: [
+//         "Логан",
+//         "Лига справедливости",
+//         "Ла-ла лэнд",
+//         "Одержимость",
+//         "Скотт Пилигрим против..."
+//     ]
+// };
+
+// const promoAdv = document.querySelectorAll(".promo__adv img"),
+//       promoBg = document.querySelector(".promo__bg"),
+//       promoGenre = promoBg.querySelector('.promo__genre'),
+//       promoInteractiveList = document.querySelector('.promo__interactive-list');
+// // с помощью стрелочной функции
+// promoAdv.forEach(item =>{
+//     item.remove();
+// });
+// // простая функция
+// // promoAdv.forEach(function(item){
+// //     item.remove();
+// // });
+
+
+// promoGenre.textContent = "ДРАМА";
+// promoBg.style.background = 'url("img/bg.jpg") center center/cover no-repeat';
+
+// promoInteractiveList.innerHTML = "";
+
+// movieDB.movies.sort();
+
+// console.log(movieDB.movies);
+
+// movieDB.movies.forEach((film, i) =>{
+//     promoInteractiveList.innerHTML += `
+//     <li class="promo__interactive-item">${i+1}. ${film}
+//         <div class="delete"></div>
+//     </li>
+//     `;
+// });
+
+//---------------------------------------------События(практика)--------------------------------------
+// const btn = document.querySelector('button');
+//       overlay = document.querySelector('.overlay');
+
+// // btn.onclick = function(){
+// //     alert("Vam BAN");            ----- в этом формате мы вызываем функцию и в дальнейшем по коду если мы сделаем обработчик он будет перекрывать первый
+// // };
+
+// // btn.addEventListener('click', ()=>{
+// //     alert('Vam BAM');                ---- жделаем оброботчик который будет в дальнешем при создании аналогичного выполнятся последовательно
+// // });
+
+// let i = 0;
+
+// const eventBtn = (e) => {
+//     console.log(e.currentTarget);
+//     console.log(e.type);
+//     // i++;
+//     // if (i ==1){
+//     //     btn.removeEventListener('click', eventBtn);
+//     // }
+
+// };
+
+// btn.addEventListener('click', eventBtn);
+// overlay.addEventListener('click', eventBtn);
+
+// const link = document.querySelector('a');
+
+// link.addEventListener('click', function(event){
+//         event.preventDefault();
+//         console.log(event.target);
+// });
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+//--------------------Работа с элементами ДОМ, получение нод, перебор, свойства-----------------------------------
+
+// console.log(document.head); - получаем все элементы хранящиеся в head
+// console.log(document.documentElement); - получаем всю html страницу
+// console.log(document.body.childNodes); - позволяет получить все ноды и узлы этого родителя элемента
+// firstChild - получить первый элемент
+// lastChild - получить последний элемент
+{/* <li>1</li> */} // - li это ДОМ элемент , а есть Дом узел - это "1"
+
+
+
+// console.log(document.querySelector("#current").parentNode.parentNode); // получили узел, мы продублировали parentNode - чтобы переместиться высше по дом дереву страницы
+// data-(обявляем) "дальше пишем что-то что нам нужно пример:modal, close" - 
+ 
+// console.log(document.querySelector('[data-current="3"]').previousSibling); // nextSibling - получить следующий элемент Notu именно ноту, previousSibling - получить предыдущий элемент Notu именно ноту
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling); получаем именно следующий элемент
+
+// console.log(document.querySelector("#current").parentElement);  получаем сразу элемент
+// console.log(document.body.firstElementChild);
+
+// console.log(document.body.childNodes); 
+//перебор нод и получение только элементы, без текстовых нод
+// for (let node of document.body.childNodes){
+//     if(node.nodeName == "#text"){
+//         continue;
+//     }
+//     console.log(node);
+// }
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------Практика работа с базой, с помощью элемнтов на странице-----------------------------------------------------------------------------------------------------------------------
+
+// document.addEventListener('DOMContentLoaded', () =>{
+//     const movieDB = {
+//         movies: [
+//             "Логан",
+//             "Лига справедливости",
+//             "Ла-ла лэнд",
+//             "Одержимость",
+//             "Скотт Пилигрим против..."
+//         ]
+//     };
+    
+//     const promoAdv = document.querySelectorAll(".promo__adv img"),
+//           promoBg = document.querySelector(".promo__bg"),
+//           promoGenre = promoBg.querySelector('.promo__genre'),
+//           promoInteractiveList = document.querySelector('.promo__interactive-list'),
+//           addForm = document.querySelector('form.add'),
+//           addInput = addForm.querySelector('.adding__input'),
+//           checkBox = addForm.querySelector('[type="checkbox"]'); // получаем через атрибут
+
+//     addForm.addEventListener('submit', (event) =>{
+//         event.preventDefault();
+
+//         let textInput = addInput.value;
+//         const favorite = checkBox.checked;
+       
+//         if(textInput){
+           
+//             if(textInput.length>=21){
+//                 textInput = `${textInput.substring(0, 22)}...`;
+//             }
+//             if(favorite == true){
+//                 textInput = "*"+textInput;
+//             }
+//             movieDB.movies.push(textInput);// пушим в базу новый фильм
+//             sortArr(movieDB.movies); // сортируем 
+//             addNewFilms(movieDB.movies, promoInteractiveList);
+//         }
+//         event.target.reset();// сбрасываем форму
+//     });
+//     // функция удаления контента на страничкес помощью стрелочной функции
+//     const deleteAdv = (arr) => {
+//         arr.forEach(item =>{
+//             item.remove();
+//         });
+//     };
+//      // простая функция
+//     // promoAdv.forEach(function(item){
+//     //     item.remove();
+//     // });
+//     //---------------------------------------
+    
+//     // функция работы с элементами на странице
+//     const editPromo = () => {
+//         promoGenre.textContent = "ДРАМА";
+//         promoBg.style.background = 'url("img/bg.jpg") center center/cover no-repeat';
+//     };
+//     //----------------------------------------
+//     //----Сортировка--------------------------
+//     const sortArr = (arr)=>{
+//         arr.sort();
+//     };
+//     //---------------------------------------
+//     // функция добавления элемента в массив базы данных
+//     function addNewFilms(films, parent){
+
+//         parent.innerHTML = "";
+        
+//         sortArr(films);
+
+//         films.forEach((film, i) =>{
+//             parent.innerHTML += `
+//             <li class="promo__interactive-item">${i+1}. ${film}
+//                 <div class="delete"></div>
+//             </li>
+//             `;
+//         });
+//         // удаление из списка фильм
+//         document.querySelectorAll('.delete').forEach((btn,i) =>{
+//             btn.addEventListener('click',()=>{
+//                 btn.parentElement.remove();
+//                 movieDB.movies.splice(i, 1);
+//                 addNewFilms(movieDB.movies, promoInteractiveList);
+//             });
+//         });
+
+//     }
+//--------------------Вызовы функций
+//    addNewFilms(movieDB.movies, promoInteractiveList);
+//    deleteAdv(promoAdv);
+//    editPromo();
+//    sortArr(movieDB.movies);
+// });
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//-------------------------------событи с мобильной весткой------------------
+
+// touchstart - срабатывает при касании к этому элементу
+// touchmove - при каждом смещении мальца будет срабатывать данное действие
+// toucheend - если палец оторвался от элемента
+// touchenter - когда ведем пальцем по экрану и наскальзываем на пределы этого элемента
+// touchleave - когда наш палец продолжил скользить по экрану за пределы этого элемента
+// touchcancel - возникет когда точка соприкосновения выходит за приделы браузера 
+
+// window.addEventListener('DOMContentLoaded', () =>{
+//     const box = document.querySelector('.box');
+//     // навесили на "div" событие которе работает только на мобильной версии
+//     // при клике на указанный "div"
+//     box.addEventListener('touchstart', (e)=>{
+//         e.preventDefault();
+
+//         console.log('Hello!!!');
+//         console.log(e.targetTouches);
+//     });
+
+//     // // навесили на "div" событие которе работает только на мобильной версии
+//     // // движении по элемменту на указанный "div"
+//     box.addEventListener('touchmove', (e)=>{
+//         e.preventDefault();
+
+//         console.log(e.targetTouches[0].pageX); // отслеживаем положение пальца
+        
+//     });
+
+//     //  // навесили на "div" событие которе работает только на мобильной версии
+//     // // отжали на указанный "div"
+//     // box.addEventListener('touchend', (e)=>{
+//     //     e.preventDefault();
+
+//     //     console.log('Stop!!!');
+//     // });
+// });
+
+// toches - это свойство выдает всех пальцев которые взаимодействуют с экраном
+// targetTouches- если нас интересует все пальцы которые взаимодействуют именно с этим элементом
+// changedTouches - список пальцев участвующих в текущем событии
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------ClassList работа с свойством, делигированние событий--------------------------------------------------
+
+// const btns = document.querySelectorAll('button'),
+//       wrapper = document.querySelector('.btn-block'); 
+// // console.log(btns[0].classList.length); - получение количества классов у элемента 
+// // console.log(btns[0].classList.item(1)); // - это метод, получить клас который распологается под индексом (указываем в скобках)
+// // console.log(btns[1].classList.add('red')); // можно добавить элементу определенный класс (указываем название в скобках)
+// // console.log(btns[0].classList.remove('blue')); // удалеине ненужного нам класса (указываем название в скобках)
+// // console.log(btns[0].classList.toggle('blue')); // позволяет тоглить классы, если класс есть у нас у элемента он будет убран если
+// // его нет то будет добавлен
+
+// // if (btns[1].classList.contains('red')){
+// //     console.log('red');
+// // }
+// // действие клик на кнопку для добавления класса или при существующем запрашивающем классе удаление его
+// btns[0].addEventListener('click', () => {
+//     // решение с помощью цыкла if вручную
+//     // if (!btns[1].classList.contains('red')){
+//     //     btns[1].classList.add('red');
+//     // } else  {
+//     //     btns[1].classList.remove('red');
+//     // }
+//     // аналогичное решение задачи с помощью toggle
+//     btns[1].classList.toggle('red');
+// });
+
+// //----------------------------Делигирование событий-----------------
+// // делаем событие на весь div но при этом проверяем что бы это была именно кнопка
+// // wrapper.addEventListener('click', (event) =>{
+// //     // console.dir(event.target); // выводим в консоль с помщью dir - чтобы увидеть в качестве объекта
+// //     if (event.target && event.target.tagName == "BUTTON"){
+// //         console.log('Hello!!');
+// //     }
+// // });
+
+// // wrapper.addEventListener('click', (event) =>{
+// //     // matches продвиннутое делигирование по силекторам которые нас интересуют
+// //     if (event.target && event.target.matches("button.red")){
+// //         console.log('Hello!!');
+// //     }
+// // });
+
+// //обычный foeEach 
+// // btns.forEach(btn =>{
+// //     btn.addEventListener('click', () =>{
+// //         console.log('Hello!!');
+// //     });
+// // });
+
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);// метод append позволит нам добавить новую созданную кнопку на wrapper
+
+//---------------------------------работа с табами на сайте, переключение с анимацией-------------------------------------
+
+// назначение глобального обработчика событий, document или window - не имеет значения 
+// внутри данного обработчика будут находиться все нужные нам события
+// window.addEventListener('DOMContentLoaded', () =>{
+
+//     const tabs = document.querySelectorAll('.tabheader__item'),
+//           tabsContent = document.querySelectorAll('.tabcontent'),
+//           tabsParent = document.querySelector('.tabheader__items'); 
+
+//     function hideTabContant(){
+//         tabsContent.forEach(item =>{
+//             item.classList.add('hide');
+//             item.classList.remove('show','fade');
+//         });
+//         // работа с класом активности
+//         tabs.forEach(item =>{
+//             item.classList.remove('tabheader__item_active'); // точку не ставим потому что и так работаем с классом
+//         });
+//     }
+
+//     // присваеваем i = 0 для того чтобы если функция вызывается поумолчанию 
+//     // мы видели сразу первый элемент функции
+//     function showTabContant(i = 0){
+//         tabsContent[i].classList.add('show','fade');// работаем не со стилями а сразу с классом, стили прописанны в отдельном файле
+//         tabsContent[i].classList.remove('hide');
+//         tabs[i].classList.add('tabheader__item_active');
+//     }
+
+//     hideTabContant();
+//     showTabContant();
+
+//     tabsParent.addEventListener('click', (event) =>{ // event - объект события
+//         const target = event.target; // связи с тем что мы часто будем использовать
+//         //данную переменную мы ее преопределяем
+        
+//         if(target && target.classList.contains('tabheader__item')){
+//             tabs.forEach((item, i)=>{
+//                 if(target == item){
+//                     hideTabContant();
+//                     showTabContant(i);
+//                 }
+//             });
+//         }
+//     }); 
+// });
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------setTimeot, скрипты и время их выполнения--------------------------------------------------------------------------
+// window.addEventListener('DOMContentLoaded', ()=>{
+
+//     // const timeOut = setTimeout(function(text){
+//     //     console.log(text);
+//     // }, 2000, 'Привет мазафака');
+// // запускаем операцию с определенным истечением времени
+// // первым аргументом определяется действие, вторым аргументов время отработки
+// // третим аргументом можно добавлять то что будет подставлятся в функцию или событие
+// // аргументов может быть бесконечное количество 
+//     const btn_new = document.querySelector('.btn');
+//     let timeOut, // создаем переменную глобально, которую можно перезаписать
+//         i = 0;
+//     //---------------ПРАКТИКА(движение элемента с интервалом)-----------
+//     function myAnimation() {
+//         const element = document.querySelector('.box');
+//         let pos = 0;
+
+//         const id = setInterval(frame, 10);
+//         function frame(){
+//             if(pos === 300){
+//                 clearInterval(id);
+//             } else {
+//                 pos++;
+//                 element.style.top = pos + 'px';
+//                 element.style.left = pos + 'px';
+//             }
+//         }
+//     }
+//      btn_new.addEventListener('click', myAnimation);
+  
+//     //-----------------------------------------------------------------
+//       // btn_new.addEventListener('click', () =>{
+//     //     // const timeOut = setTimeout(logger, 2000);
+//     //     // return timeOut;
+//     //     timeOut = setInterval(logger, 500);
+//     // });
+//     // чем лучше рекрусивный setTimeout чем setInterval - если функция работает дольше интервала который мы указали 
+//     // то в дальнейшем setInterval игнорирует указанный период времени для интервала.
+//     // setTimeout - строго дожидается выполнения своей функции, а только после отрабатывает дальше.
+    
+//     // clearInterval(timeOut);// - остановили таймер 
+
+//     // function logger(){
+//     //     if(i === 3){
+//     //         clearInterval(timeOut);
+//     //     }
+//     //     console.log('Привет мазафака');
+//     //     i++;
+//     // }
+// });
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------работа с методом Date()-----------------------------------------------------------------------------
+// window.addEventListener('DOMContentLoaded', ()=>{
+//     // const now = new Date(); // мы получили 6 месяц потомучто месяцы считаются с 0
+//     // методы get
+//     // timeStemp - хранится в милисикундах, удобно манипулировать, трансформировать милисекунды в дату и наоборот
+//     // начало отчета 1970 год, отрицательное значение в методе Data() дает возможность обратиться к дате меньше 1970 года
+//     // год всегда состоит из 4 цыфр
+//     // console.log(now.getFullYear()); // - метод получения года даты
+//     // console.log(now.getMonth()); //- метод получения месяца даты
+//     // console.log(now.getDate()); // - метод получения день даты
+//     // console.log(now.getDay()); // - день недели
+//     // console.log(now.getHours()); // - день недели
+//     // console.log(now.getUTCHours()); // - время по UTC
+    
+//     // console.log(now.getTimezoneOffset()); // - получаем разничу по времени с местным временем и UTC
+//     // console.log(now.getTime()); // - получаем время прошедшее с 1970 по настоящее время (количество милисекунд)
+
+//     // методы set (все методы как и get, кроме timezoneoffset)
+//     // console.log(now.setHours(40));// если указать дату не правильно то метод сам себя исправит и покажет
+//     // console.log(now);
+
+//     // let start = new Date();
+
+//     // for(let i = 0; i<100000; i++){
+//     //     let some = i ** 3; // "** n"- возводит в степень которую мы укажем
+//     // }
+
+//     // let end = new Date();
+
+//     // alert(`Цыкл отработал за ${end-start} миллисекунд`);
+  
+// });
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
